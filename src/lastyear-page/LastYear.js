642f303e06ffc6b22969678125cf4bd6264f252c
koiralasasbit <koiralasasbit@gmail.com>
2022-06-11 10:11:57 -0400

    scambi 2021 highlights

--- src/lastyear-page/LastYear.js
+++ src/lastyear-page/LastYear.js
@@ -6,16 +6,23 @@ class LastYear extends Component {
     return (
       <div className="lastyear-background">
         <div className="lastyear-main">
-          <div className="lastyear-text">Last year at Scambi</div>
-          <div className="rectangle34"></div>
-          <div className="rectangle35"></div>
-          <div className="rectangle26"></div>
-          <div className="rectangle33"></div>
-          <div className="rectangle31"></div>
-          <div className="rectangle28"></div>
-          <div className="rectangle32"></div>
-          <div className="rectangle30"></div>
-          <div className="rectangle29"></div>
+          <div className="photo-gallery">
+            <div className="lastyear-text">Scambi 2021 Highlights</div>
+            <div className="rectangle34"></div>
+            <div className="rectangle35"></div>
+            <div className="rectangle26"></div>
+            <div className="rectangle33"></div>
+            <div className="rectangle31"></div>
+            <div className="rectangle28"></div>
+            <div className="rectangle32"></div>
+            <div className="rectangle30"></div>
+            <div className="rectangle29"></div>
+          </div>
+          <div className="explore-button">
+            <div className="explore-label">
+              Explore the 2021 Edition: “Meeting”
+            </div>
+          </div>
         </div>
       </div>
     );
