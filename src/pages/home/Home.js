6eb140b3b52ccc189fa0950ae4b20f866839f933
traciingu <traciinguyen@hotmail.com>
2022-07-21 15:31:02 -0400

    Fix sizing for TableTalk image

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -41,7 +41,7 @@ const Home = () => {
 						ideas.
 					</p>
 				</div>
-				<div className='flex-image'>
+				<div className='w-1/4'>
 					<img src={TableTalk} alt='' />
 				</div>
 			</Flex>
