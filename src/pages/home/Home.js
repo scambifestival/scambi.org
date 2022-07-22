521e1a61b78e26135536462f3220f7f431a4dd4d
traciingu <traciinguyen@hotmail.com>
2022-07-22 15:00:50 -0400

    Make purpose statement reponsive to smaller screens

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -179,14 +179,14 @@ const Home = () => {
 				</div>
 			</Flex>
 
-			<div className='flex justify-center px-14 py-10 font-normal bg-white sm:px-20 lg:px-24 xl:px-42 2xl:px-60'>
-				<p className='text-3xl sm:leading-relaxed sm:text-4xl xl:text-5xl xl:leading-relaxed 2xl:leading-loose'>
+			<div className='flex justify-center px-10 py-10 font-normal bg-white sm:px-20 lg:px-24 xl:px-42 2xl:px-60'>
+				<p className='text-2xl sm:leading-relaxed sm:text-4xl xl:text-5xl xl:leading-relaxed 2xl:leading-loose'>
 					“Our purpose is to recover the value of{' '}
-					<span className='font-bold text-3xl sm:text-4xl xl:text-5xl'>
+					<span className='font-bold text-2xl sm:text-4xl xl:text-5xl'>
 						communication
 					</span>{' '}
 					and{' '}
-					<span className='font-bold text-3xl sm:text-4xl xl:text-5xl'>
+					<span className='font-bold text-2xl sm:text-4xl xl:text-5xl'>
 						meeting
 					</span>
 					, often forgotten or hidden behind a two-dimentional screen. Scambi is
